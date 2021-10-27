98f193e5263cecdb64a6ddc9b13418d0a76d70ec
Alex-Reyne <creativereyne@gmail.com>
2021-10-27 16:28:26 -0600

    add test functions to letterPositions and create a base function with expected results

---
+++ letterPositions.js
@@ -0,0 +1,55 @@
+let eqArrays = function(arr1, arr2) {
+  let stringOne = '';
+  let stringTwo = '';
+  let isTrue = false
+
+  for (let i of arr1) {
+    stringOne += i;
+  }
+
+  for (let j of arr2) {
+    stringTwo += j;
+  }
+
+  if (stringOne === stringTwo) {
+    isTrue = true;
+  }
+  
+  return isTrue;
+}
+
+const assertEqualArray = function(arr1, arr2) {
+  
+  if (eqArrays(arr1, arr2)) {
+    console.log(`👌 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+let string = "hello";
+
+assertEqualArray(eqArrays(string), "hello")
+
+// const letterPositions = function(sentence) {
+//   const results = {};
+
+
+//   return results;
+// }
+
+// "lighthouse in the house"
+-----------------------------------
+// let obj ={ 
+//   l: [0],
+//   i: [1, 11],
+//   g: [2],
+//   h: [3, 5, 15, 18],
+//   t: [4, 14],
+//   o: [6, 19],
+//   u: [7, 20],
+//   s: [8, 21],
+//   e: [9, 16, 22],
+//   n: [12]
+// }
