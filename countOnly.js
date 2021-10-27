b7d39da062f2a15da8dc86beaf146bb681176e5f
Alex-Reyne <creativereyne@gmail.com>
2021-10-27 16:03:56 -0600

    created program that counts the number of times specific items show up in an array.

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`👌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let result = {}
+
+  for (let item of allItems){
+    for (let name in itemsToCount) {
+      if (name === item && itemsToCount[name] === true) {
+        result.hasOwnProperty(name) ?
+        result[name] += 1 :
+        result[name] = 1;
+      }
+    } 
+  }
+
+  return result;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false })
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
