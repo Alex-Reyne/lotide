2246972399e30bd2f400213a53f77fe9ee8345c5
Alex-Reyne <creativereyne@gmail.com>
2021-10-26 18:28:39 -0600

    create a progam that check if two arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`👌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+let eqArrays = function(arr1, arr2) {
+  let stringOne = '';
+  let stringTwo = '';
+  let isTrue = false
+
+  for (let i of arr1) {
+    stringOne += i;
+  }
+
+  for (let j of arr2) {
+    stringTwo += j;
+  }
+
+  if (stringOne === stringTwo) {
+    isTrue = true;
+  }
+  
+  return isTrue;
+}
+
+assertEqual(eqArrays([4, 5, 6], [4, 5, 6]), true);
