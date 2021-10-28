59790c61f681909358286b25927c4a6a93c6a538
Alex-Reyne <creativereyne@gmail.com>
2021-10-27 18:48:02 -0600

    create test cases for eqObjects function

--- eqObjects.js
+++ eqObjects.js
@@ -13,3 +13,12 @@ const assertEqual = function(actual, expected) {
 const eqObjects = function(object1, object2) {
 
 };
+
+const ab = {a: "1", b: "2"};
+const ba = {b: "2", a: "1"};
+
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = {a: "1", b: "2", c: "3"};
+
+assertEqual(eqObjects(abc, ab), false);
