0df584c415376ad4f4c0124c4b0ad8b7ac61cbfb
Alex-Reyne <creativereyne@gmail.com>
2021-10-28 15:50:40 -0600

    inster test functions to check map function

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-const words = ["ground", "control", "to", "major", "tom"];
+const words = ['123', '456', '789'];
 
 const map = function(array, callback) {
   const results = [];
@@ -12,3 +12,35 @@ const map = function(array, callback) {
 const results1 = map(words, word => word[0]);
 
 console.log(results1);
+
+let eqArrays = function(arr1, arr2) {
+  let stringOne = '';
+  let stringTwo = '';
+  let isTrue = false
+
+  for (let i of arr1) {
+    stringOne += i;
+  }
+
+  for (let j of arr2) {
+    stringTwo += j;
+  }
+
+  if (stringOne === stringTwo) {
+    isTrue = true;
+  }
+  
+  return isTrue;
+}
+
+const assertEqualArray = function(arr1, arr2) {
+  
+  if (eqArrays(arr1, arr2)) {
+    console.log(`👌 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+assertEqualArray(results1, [1, 4, 7]);
