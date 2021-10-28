4dcdb66ddeb74d631eae6a89d913c1460aed1612
Alex-Reyne <creativereyne@gmail.com>
2021-10-27 20:04:28 -0600

    fix function to return the right values

--- eqObjects.js
+++ eqObjects.js
@@ -8,17 +8,58 @@ const assertEqual = function(actual, expected) {
 
 };
 
+let eqArrays = function(arr1, arr2) {
+  let stringOne = '';
+  let stringTwo = '';
+  let isTrue = false
+
+  for (let i of arr1) {
+    stringOne += i;
+  }
+
+  for (let j of arr2) {
+    stringTwo += j;
+  }
+
+  if (stringOne === stringTwo) {
+    isTrue = true;
+  }
+  
+  return isTrue;
+}
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwie you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  let object1Keys = Object.keys(object1);
+  let object2Keys = Object.keys(object2);
 
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+
+  for (let element of object1Keys) {
+    console.log(element, object1[element], element, object2[element]);
+    if (object1[element] !== object2[element]) {
+      return false;
+    }
+     
+  }
+  return true;
 };
 
-const ab = {a: "1", b: "2"};
-const ba = {b: "2", a: "1"};
+// const ab = {a: "1", b: "2"};
+// const ba = {b: "2", a: "1"};
 
-assertEqual(eqObjects(ab, ba), true);
+// assertEqual(eqObjects(ab, ba), true);
 
-const abc = {a: "1", b: "2", c: "3"};
+// const abc = {a: "1", b: "2", c: "3"};
 
-assertEqual(eqObjects(abc, ab), false);
+// assertEqual(eqObjects(abc, ab), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
