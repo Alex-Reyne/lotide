62d3f653eb48927b7596efabd4443f6bf7afb378
Alex-Reyne <creativereyne@gmail.com>
2021-11-01 15:11:31 -0600

    install mocha chai. implement mocha chai into head.test.js

--- test/head.test.js
+++ test/head.test.js
@@ -1,5 +1,15 @@
-const assertEqual = require('../assertEqual');
 const head = require('../head.js')
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert
+
+describe('#head returns ONLY the first item in an array', () => {
+
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
